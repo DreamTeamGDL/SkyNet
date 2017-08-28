075524d017ab98b7998aaa83d65699d4c0e66162
christopher <a01225198@itesm.mx>
2017-08-28 13:06:17 -0500

    Getting byte[] from db

--- SkyNet/app/src/main/java/gdl/dreamteam/skynet/Others/IDataRepository.kt
+++ SkyNet/app/src/main/java/gdl/dreamteam/skynet/Others/IDataRepository.kt
@@ -4,5 +4,5 @@ package gdl.dreamteam.skynet.Others
  * Created by christopher on 28/08/17.
  */
 interface IDataRepository {
-    fun getZone(name: String): Zone
+    fun getZone(name: String): Zone?
 }
--- SkyNet/app/src/main/java/gdl/dreamteam/skynet/Others/SQLRepository.kt
+++ SkyNet/app/src/main/java/gdl/dreamteam/skynet/Others/SQLRepository.kt
@@ -1,29 +1,46 @@
 package gdl.dreamteam.skynet.Others
 
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 import com.google.gson.Gson
+import java.nio.ByteBuffer
+import java.util.*
 
 /**
  * Created by christopher on 28/08/17.
  */
-class SQLRepository : IDataRepository {
+class SQLRepository (
+    context: Context
+) : IDataRepository, SQLiteOpenHelper (
+    context,
+    database,
+    null,
+    version
+) {
+    companion object {
+        const val database = "database.db"
+        const val version = 1
+    }
 
-    override fun getZone(name: String): Zone {
-        var zone = Zone(
-            "zone1",
-            arrayOf(Client(
-                "room1",
-                "aosiherwela",
-                arrayOf(Device(
-                    "ailesjrwenf",
-                    "fan",
-                    Fan(25.2f, 0.45f, 1)
-                ))
-            ))
-        )
-        (zone.clients[0].devs[0].data as Fan).status = true
-        var gson = Gson()
-        println(gson.toJson(zone))
-        return zone
+    override fun onCreate(db: SQLiteDatabase?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getZone(name: String): Zone? {
+        var query = "Select data From Zones Where name = $name"
+        val cursor = readableDatabase.rawQuery(query, null)
+        if (cursor.moveToFirst()) {
+            val rawJson: ByteArray = cursor.getBlob(0)
+            var gson = Gson()
+            val json = Charsets.UTF_8.decode(ByteBuffer.wrap(rawJson))
+            return gson.fromJson(json.toString(), Zone::class.java)
+        }
+        return null
     }
 
 }
