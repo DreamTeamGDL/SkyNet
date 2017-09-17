8a56f1f6ac51a9ab8e9689e0f3df64519b3f9f44
eldelcesar <cesarjr.espinosa@gmail.com>
2017-09-17 11:53:14 -0500

    UI v1.0 - Main activities complete

---
+++ SkyNet/app/src/main/java/gdl/dreamteam/skynet/Activities/ClientsActivity.java
@@ -0,0 +1,15 @@
+package gdl.dreamteam.skynet.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import gdl.dreamteam.skynet.R;
+
+public class ClientsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clients);
+    }
+}
---
+++ SkyNet/app/src/main/java/gdl/dreamteam/skynet/Activities/ZonesActivity.kt
@@ -0,0 +1,14 @@
+package gdl.dreamteam.skynet.Activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import gdl.dreamteam.skynet.R
+
+class ZonesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_zones)
+    }
+}
